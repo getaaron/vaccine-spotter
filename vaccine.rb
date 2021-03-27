d35af8563fabb17b10860a068376fa1f10b8f138
Sean Coburn <seandcoburn@users.noreply.github.com>
2021-03-26 17:23:39 -0700

    Additional comments and tweaks

--- vaccine.rb
+++ vaccine.rb
@@ -8,16 +8,16 @@ require 'os'
 # Set these variables
 state = 'CA'
 zips = %w[94022 94024 94040 94043 94085 94086 94087 94089 94301 94303 94304 94305 94306 95008 95014 95020 95030 95032 95033 95035 95037 95050 95051 95054 95070 95110 95111 95112 95116 95118 95119 95120 95121 95123 95124 95125 95126 95127 95128 95129 95131 95132 95133 95135 95136 95138 95148]
-#zip codes of your county or where you're willing to drive to an appointment
+# zip codes of your county or where you're willing to drive to an appointment
 
-#don't touch these
+# create the 'api' URL based on the user's state
 vs_url = 'https://www.vaccinespotter.org/api/v0/states/' + state + '.json'
 
 while true
   # grab latest JSON as array of hashes
   features = JSON.parse(HTTParty.get(vs_url).body)['features']
 
-  # find matches to county_zips AND appointments_available: true
+  # find matches to county_zips AND appointments_available: true -- remove && properties['provider_brand_id'] == 40 if you want other providers than Rite Aid
   matches = features.select { |feature|
     properties = feature['properties']
     zips.include?(properties['postal_code']) && properties['appointments_available'] == true && properties['provider_brand_id'] == 40
